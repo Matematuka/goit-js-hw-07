5b85e104fbe481c40cfb8369ffac8f94e7dcadaf
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-01-02 18:06:31 +0200

    Task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,22 @@
+// Напиши скрипт, який змінює колір фону елемента < body > через
+// інлайн - стиль по кліку на button.change - color і задає це значення 
+// кольору текстовим вмістом для span.color.
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btn = document.querySelector(".change-color");
+const bodyElem = document.querySelector("body");
+const textSpan = document.querySelector("span.color");
+
+btn.addEventListener("click", handleClick);
+
+function handleClick(evt) {
+  bodyElem.style.backgroundColor = getRandomHexColor();
+  textSpan.textContent = getRandomHexColor();
+}
+
