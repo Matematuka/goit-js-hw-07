f07fa9c9f08645526858b3a9697e176ff7dc7e9d
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-01-04 21:33:36 +0200

    Task-6

--- js/task-6.js
+++ js/task-6.js
@@ -12,22 +12,22 @@
 //     .padStart(6, 0)}`;
 // }
 
-const createBtn = document.querySelector("button[data-create]");
-const destroyBtn = document.querySelector("button[data-destroy]");
-const myInput = document.querySelector("#controls").firstElementChild;
+// const createBtn = document.querySelector("button[data-create]");
+// const destroyBtn = document.querySelector("button[data-destroy]");
+// const myInput = document.querySelector("#controls").firstElementChild;
 
-myInput.addEventListener("input", handleInput);
-function handleInput(evt) {
-  const count = evt.target.value;
-}
+// myInput.addEventListener("input", handleInput);
+// function handleInput(evt) {
+//   const count = evt.target.value;
+// }
 
 
-function createBoxes(amount) {
-  if (amount >= 1 && amount <= 100)
-  {
+// function createBoxes(amount) {
+//   if (amount >= 1 && amount <= 100)
+//   {
 
-  }
-}
+//   }
+// }
  
 // createBtn.addEventListener("click", handleClick);
 // function handleClick(evt) {
@@ -35,3 +35,49 @@ function createBoxes(amount) {
 //   evt.target.reset();
 //  }
 
+const num = document.querySelector("input");
+const btnCreate = controls.querySelector("button[data-create]");
+const btnDestroy = controls.querySelector("button[data-destroy]");
+const boxes = document.querySelector("#boxes");
+btnCreate.addEventListener("click", () => {
+  let amount = num.value;
+  createBoxes(amount);
+});
+function createBoxes(amount) {
+  let wid = 30;
+  let hig = 30;
+  let a = 1;
+  destroyBoxes();
+  const fragment = document.createDocumentFragment();
+  if (amount >= 1 && amount <= 100) {
+    for (let i = 0; i < amount; i++) {
+      if (i > 0) {
+        wid += 10;
+        hig += 10;
+        a += 1;
+      }
+      let item = document.createElement("div");
+      item.style.width = wid + "px";
+      item.style.height = hig + "px";
+      item.textContent = a;
+      item.style.textAlign = "center";
+      item.style.backgroundColor = getRandomHexColor();
+      fragment.appendChild(item);
+    }
+    boxes.appendChild(fragment);
+  }
+  num.value = "";
+}
+btnDestroy.addEventListener("click", () => {
+  destroyBoxes();
+});
+function destroyBoxes() {
+  while (boxes.firstChild) {
+    boxes.removeChild(boxes.firstChild);
+  }
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
