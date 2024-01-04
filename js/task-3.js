d98dd3624d1b43a60d6ab1329f62a8a8ac55e48e
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-01-04 19:09:32 +0200

    EditTask-3

--- js/task-3.js
+++ js/task-3.js
@@ -4,7 +4,9 @@ textInput.addEventListener("input", (evt) => {
     if (evt.currentTarget.value.trim() === "") {
         greet.textContent === "Anonymous";
     } else {
-        greet.textContent = evt.currentTarget.value.trim();
+        greet.textContent = evt.currentTarget.value.trim();   
     }
 });
 
+const hello = document.querySelector("h1");
+hello.classList.add("head");
